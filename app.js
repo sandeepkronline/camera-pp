2fe944a97a235e37bfcc4b28b45f0d28fdca1d1d
sandeepkronline <91860092+sandeepkronline@users.noreply.github.com>
2021-10-06 17:57:48 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function loadMarkers(){
 		const markerDiv = document.createElement("a-gltf-model");
 		markerDiv.setAttribute("gltf-model", "trex/skyscraper.gltf");
 		markerDiv.setAttribute("scale", "5 5 5");
-		markerDiv.setAttribute("position", "50 0 0 );
+		markerDiv.setAttribute("position", "50 0 0" );
 		var element = document.getElementById("marker");
 		element.appendChild(markerDiv);
 	}
