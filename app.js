1f12e9c150b0cb9fca657dea765017be4ffdd851
sandeepkronline <91860092+sandeepkronline@users.noreply.github.com>
2021-10-06 18:31:03 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function loadMarkers(){
 	if( trakingId = "trex"){
 		
 		const markerDiv = document.createElement("a-gltf-model");
-		markerDiv.setAttribute("gltf-model", "trex/skyscraper.gltf");
+		markerDiv.setAttribute("gltf-model", "trex/scene.gltf");
 		markerDiv.setAttribute("scale", "5 5 5");
 		markerDiv.setAttribute("position", "50 0 0" );
 		var element = document.getElementById("#marker");
@@ -16,7 +16,7 @@ function loadMarkers(){
 	}
 	else{
 		const markerDiv = document.createElement("a-gltf-model");
-		markerDiv.setAttribute("gltf-model", "trex/scene.gltf");
+		markerDiv.setAttribute("gltf-model", "trex/skyscraper.gltf");
 		markerDiv.setAttribute("scale", "5 5 5");
 		markerDiv.setAttribute("position", "50 50 0" );
 		markerDiv.setAttribute("rotation", "90 270 0" );
