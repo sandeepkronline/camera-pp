5f0408847dba872bf978ad163a5a9d085aed7695
Sandeep KR <sandeep.kr@trenser.com>
2021-10-06 16:39:49 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ function loadMarkers(){
 	const urlParams = new URLSearchParams(window.location.search)
 	let marker = urlParams.get("id")
 	console.log(marker);
+	console.log(urlParams.get("marker"));
 	
 	if( marker = "trex"){
-		var element = document.getElementById(id);
+		var element = document.getElementById("marker");
 		const newDiv = document.createElement("a-gltf-model", { gltf-model: "trex/skyscraper.gltf",  scale : "5 5 5", position : "50 0 0" , rotation:"0 90 0"});
 	}
 	else{
