d95618d2f35bc1948af30ff0f23aee8293930d82
Sandeep KR <sandeep.kr@trenser.com>
2021-10-08 15:54:08 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,104 @@
 function loadMarkers(){
+	// Get Query Parameters
 	const urlParams = new URLSearchParams(window.location.search)
-	let trakingId = urlParams.get("id")
-	let markerId = urlParams.get("marker")
-	console.log(trakingId);
-	console.log(markerId);
+	let imageId = urlParams.get("image")
+	let modelId = urlParams.get("model")
+	let modelType= urlParams.get("type")
+	console.log(imageId);
+	console.log(modelId);
+	console.log(modelType);
 	
-	if( trakingId = "trex"){
+
+	
+	
+	// Upadate tracking image URL
+	let baseUrl = "https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/sandeepkronline/webar/main/track/";
+	let nftUrl = baseUrl + imageId + "/" + imageId;
+	console.log(nftUrl);
+	var nftNode = document.getElementById("#marker");
+	nftNode.setAttribute("url", nftUrl);
+	
+	if( modelType == "3d"){
+		load3dModles();
+	}
+	else if( modelType == "2d"){
+		loadImages();
+	}
+	else if( modelType == "text")
+	{
+		loadText()
+	}
+
+
 		
-		const newDiv = document.createElement("a-gltf-model", { "gltf-model": "trex/skyscraper.gltf",  "scale" : "5 5 5", "position" : "50 0 0" , "rotation":"0 90 0"});
-		var element = document.getElementById("marker");
-		element.appendChild(newDiv);
+	//https://carnaux.github.io/NFT-Marker-Creator/
+	//https://console.echo3d.co/#/pages/contentmanager
+}
+
+function load3dModles(){
+	const urlParams = new URLSearchParams(window.location.search)
+	let modelId = urlParams.get("model")
+	
+	const markerDiv = document.createElement("a-gltf-model");
+	markerDiv.setAttribute("material","transparent:true;shader:flat;side:double;");
+	markerDiv.setAttribute("rotation", "0 0 0" );
+	markerDiv.setAttribute("position", "0 0 0" );
+	if( modelId == "1"){
+		markerDiv.setAttribute("gltf-model", "models/3D/melite.gltf");
+		markerDiv.setAttribute("scale", "50 50 50");
+		//markerDiv.setAttribute("position", "100 -300 0" );	
+	}
+	else if (modelId == "2"){
+		markerDiv.setAttribute("gltf-model", "models/3D/skyscraper.gltf");
+		markerDiv.setAttribute("scale", "10 10 10");
+		//markerDiv.setAttribute("position", "100 -300 0" );
+		//markerDiv.setAttribute("rotation", "0 0 0" );	
 	}
 	else{
-		var element = document.getElementById(id);
+		markerDiv.setAttribute("gltf-model", "models/3D/valentim.gltf");
+		markerDiv.setAttribute("scale", "10 10 10");
+		//markerDiv.setAttribute("position", "100 -300 0" );
+		//markerDiv.setAttribute("rotation", "0 45 45" );
 		
 	}
-		
-	
+	var element = document.getElementById("#marker");
+	element.appendChild(markerDiv);
+}
+
+function loadImages(){
+	const urlParams = new URLSearchParams(window.location.search)
+	let modelId = urlParams.get("model")
+	const markerDiv = document.createElement("a-image");
+	markerDiv.setAttribute("rotation", "0 0 0" );
+	markerDiv.setAttribute("position", "0 0 0" );
+	if( modelId == "1"){
+		markerDiv.setAttribute("src", "models/Image/butterflies.gif");
+		markerDiv.setAttribute("scale", "100 100 100");
+		//markerDiv.setAttribute("rotation", "-90 0 0");	
+		//markerDiv.setAttribute("position", "100 -300 0" );
+	}else{
+		markerDiv.setAttribute("src", "models/Image/love.png");
+		markerDiv.setAttribute("scale", "100 100 100");
+		//markerDiv.setAttribute("rotation", "-90 0 0");	
+		//markerDiv.setAttribute("position", "100 -300 0" );
+	}
+	var element = document.getElementById("#marker");
+	element.appendChild(markerDiv);	
+}
+
+
+function loadText(){
+	const urlParams = new URLSearchParams(window.location.search)
+	let modelId = urlParams.get("model")
+	const markerDiv = document.createElement("a-text");
+	markerDiv.setAttribute("value", modelId);
+	markerDiv.setAttribute("scale", "500 500 500");
+	markerDiv.setAttribute("color", "black");
+	//markerDiv.setAttribute("rotation", "0 0 45");
+	//markerDiv.setAttribute("position", "100 -100 50" );
+	markerDiv.setAttribute("rotation", "0 0 0" );
+	markerDiv.setAttribute("position", "0 0 0" );
+	var element = document.getElementById("#marker");
+	element.appendChild(markerDiv);	
 }
 
